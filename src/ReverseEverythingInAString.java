2008ac1604680033dbfb6651c50279d58e4ac6f9
thesillycoder <thesillycoder@users.noreply.github.com>
2015-08-23 21:31:04 -0400

    Update ReverseEverythingInAString.java

--- src/ReverseEverythingInAString.java
+++ src/ReverseEverythingInAString.java
@@ -18,7 +18,10 @@ public class ReverseEverythingInAString {
 		if ((str == null) || (str.length() <= 1)) {
 			return str;
 		}
-		return reverse(str.substring(1)) + str.charAt(0);
+		StringBuilder sb = new StringBuilder();
+		sb.append(reverse(str.substring(1)));
+		sb.append(str.charAt(0));
+		return sb.toString();
 	}
 
 	public static void main(String[] args) {
